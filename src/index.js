84a8c3cb8de94372da6a139d078b375bc0fa4251
MigenaPl <141563111+MigenaPl@users.noreply.github.com>
2023-09-14 17:15:04 +0200

    checkpoint 14/09/23 overwrite(?)

--- src/index.js
+++ src/index.js
@@ -20,30 +20,18 @@ function updateTime() {
   tokyoTimeElement.innerHTML = tkLocalTime.format(
     "hh:mm:ss [<small>]A[</small>]"
   );
-
-  //SGP
-  let singaporeElement = document.querySelector("#singapore");
-  let singaporeDateElement = singaporeElement.querySelector(".date");
-  let singaporeTimeElement = singaporeElement.querySelector(".time");
-  let sgLocalTime = moment().tz("Asia/Singapore");
-
-  singaporeDateElement.innerHTML = sgLocalTime.format("MMMM Do YYYY");
-  singaporeTimeElement.innerHTML = sgLocalTime.format(
-    "hh:mm:ss [<small>]A[</small>]"
-  );
 }
 
 //dropdown select
 function updateCity(event) {
-  setInterval(() => {
-    let cityTimeZone = event.target.value;
-    if (cityTimeZone === "current") {
-      cityTimeZone = moment.tz.guess();
-    }
-    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-    let cityTime = moment().tz(cityTimeZone);
-    let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = `
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
   <div class="city">
         <div>
           <h2>${cityName}</h2>
@@ -55,7 +43,6 @@ function updateCity(event) {
           </div>
           <a href="/">All cities</a>
       `;
-  }, 1000);
 }
 
 //update time call
@@ -65,3 +52,26 @@ setInterval(updateTime, 1000);
 //update city call
 let citiesSelect = document.querySelector("#city-select");
 citiesSelect.addEventListener("change", updateCity);
+
+//add city function
+function addCity (event) {
+let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML += `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+}
+let addButton = document.querySelector("#add-button");
+addButton.addEventListener("click", addCity);
